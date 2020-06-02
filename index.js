4280a9a5c661a102e36a2111869d1d7dd58f6cc8
tmd <tmdaodieu@gmail.com>
2020-06-02 21:38:01 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sumit(){
-    const name =document.getElementById("nameWork").value
-    const nd =document.getElementById("contentWork").value
+    const nameWork =document.getElementById("nameWork").value
+    const contentWork =document.getElementById("contentWork").value
     const timeStart =document.getElementById("timeStart").value
     const timeEnd =document.getElementById("timeEnd").value
     
